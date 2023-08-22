9ba092513fb026414f9c6b7bd410833d4d5f543a
Brian Kelly <polymonic@gmail.com>
2023-08-21 21:21:30 -0500

    Add Jackbox Games and the ability only show free to host games

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import games from "./games";
 
 const App = () => {
     const [players, setPlayers] = useState<number>(2);
+    const [showOnlyFreeToHost, setShowOnlyFreeToHost] = useState<boolean>(true);
 
     const filteredGames = useMemo(
         () =>
-            games.filter((game) => {
-                return players >= game.minimumPlayers && (!game.maximumPlayers || players <= game.maximumPlayers);
-            }),
-        [players]
+            games.filter((game) =>
+                players >= game.minimumPlayers
+                && (!game.maximumPlayers || players <= game.maximumPlayers)).filter((game) => showOnlyFreeToHost ? game.freeToHost : true).sort((a, b) => a.name > b.name ? 1 : -1),
+        [players, showOnlyFreeToHost]
     );
 
     return (
@@ -22,9 +23,17 @@ const App = () => {
             <fieldset>
                 <legend>Filters:</legend>
 
-                <label htmlFor="players"> Number of Players</label>
-                <input id="players" type="number" min="1" max="50" value={players}
-                       onChange={(event) => setPlayers(Number.parseInt(event.target.value))}/>
+                <p>
+                    <label htmlFor="players"> Number of Players</label>
+                    <input id="players" type="number" min="1" max="50" value={players}
+                           onChange={(event) => setPlayers(Number.parseInt(event.target.value))}/>
+                </p>
+
+                <p>
+                    <input id="freeToHost" type="checkbox" checked={showOnlyFreeToHost}
+                           onClick={(event) => setShowOnlyFreeToHost(!showOnlyFreeToHost)}/>
+                    <label htmlFor="freeToHost">Show Only Free to Host Games</label>
+                </p>
             </fieldset>
 
             {filteredGames.map(game =>
@@ -32,6 +41,7 @@ const App = () => {
                     <h2><a href={game.url}>{game.name}</a> <small className="text-muted">
                         {!game.maximumPlayers && <>{game.minimumPlayers}+ players</>}
                         {game.maximumPlayers && <>{game.minimumPlayers} to {game.maximumPlayers} players</>}
+                        {game.freeToHost && <>, Free to Host</>}
                     </small></h2>
                     <p>{game.description}</p>
                 </>)}
