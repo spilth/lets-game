ae74eabfb901ee55da6a885841686e9ef3fc71b2
Brian Kelly <polymonic@gmail.com>
2023-08-21 20:35:57 -0500

    Basic functionality of displaying games based on the number of players

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useMemo, useState} from 'react';
 import './App.css';
+import games from "./games";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [players, setPlayers] = useState<number>(2);
+
+    const filteredGames = useMemo(
+        () =>
+            games.filter((game) => {
+                return players >= game.minimumPlayers && (!game.maximumPlayers || players <= game.maximumPlayers);
+            }),
+        [players]
+    );
+
+    return (
+        <div className="container">
+            <h1>Let's Game!</h1>
+
+            <p>A collection of games you can easily play online with others.</p>
+
+            <fieldset>
+                <legend>Filters:</legend>
+
+                <label htmlFor="players"> Number of Players</label>
+                <input id="players" type="number" min="1" max="50" value={players}
+                       onChange={(event) => setPlayers(Number.parseInt(event.target.value))}/>
+            </fieldset>
+
+            {filteredGames.map(game =>
+                <>
+                    <h2><a href={game.url}>{game.name}</a> <small className="text-muted">
+                        {!game.maximumPlayers && <>{game.minimumPlayers}+ players</>}
+                        {game.maximumPlayers && <>{game.minimumPlayers} to {game.maximumPlayers} players</>}
+                    </small></h2>
+                    <p>{game.description}</p>
+                </>)}
+        </div>
+    );
+};
 
 export default App;
---
+++ src/games.ts
@@ -0,0 +1,82 @@
+const games = [
+    {
+        "name": "Codenames",
+        "description": "Codenames is a word-based party game where players split into two teams and take turns giving one-word clues to help their teammates identify specific words on a grid of cards. The goal is to find all of your team's words while avoiding the opposing team's words and the game's \"assassin\" word.",
+        "url": "https://codenames.game/",
+        "minimumPlayers": 4
+    },
+    {
+        "name": "Connect the Stars",
+        "description": "A cooperative puzzle game where players work together to connect stars on a grid using a limited set of pathways. The goal is to create a clear path that connects all the stars while avoiding obstacles and efficiently utilizing the available pathways.",
+        "url": "https://connectthestars.xyz",
+        "minimumPlayers": 1
+    },
+    {
+        "name": "Draw Battle",
+        "description": "A competitive multiplayer game that challenges players' artistic and guessing skills. Participants take turns drawing prompts while others attempt to guess what the drawing represents in a limited amount of time, leading to amusing and unpredictable outcomes.",
+        "url": "https://drawbattle.io/",
+        "minimumPlayers": 2
+    },
+    {
+        "name": "Gartic Phone",
+        "description": "A multiplayer game that combines drawing and the classic \"telephone\" game concept. Players take turns drawing a prompt or describing an image, and each subsequent player must interpret and convey the content in their own way, resulting in hilarious and often distorted outcomes.",
+        "url": "https://garticphone.com",
+        "minimumPlayers": 4,
+        "maximumPlayers": 50
+    },
+    {
+        "name": "GeoGuessr",
+        "description": "A geographical exploration game where players are placed in random locations around the world using Google Street View. They must use visual clues to guess their location's country, city, or region, earning points based on the accuracy of their guesses and discovering unique and captivating places along the way.",
+        "url": "https://www.geoguessr.com/",
+        "minimumPlayers": 1
+    },
+    {
+        "name": "Hanab",
+        "description": "A cooperative card game where players work together to create a perfect fireworks display. The catch is that each player holds their cards facing away from them, requiring teammates to give limited information to help each other play the right cards in the right order.",
+        "url": "https://www.hanab.cards/en",
+        "minimumPlayers": 2,
+        "maximumPlayers": 5
+    },
+    {
+        "name": "HaxBall",
+        "description": "A fast-paced online multiplayer soccer game that features simple physics and controls. Players control circular avatars and work together in teams to score goals, making it an accessible and engaging sports game for both casual and competitive players.",
+        "url": "https://www.haxball.com/",
+        "minimumPlayers": 2,
+        "maximumPlayers": 20
+    },
+    {
+        "name": "Longwave",
+        "description": "A social guessing game where players try to gauge how well their teammates can align their thoughts. One player gives a clue that falls on a spectrum, and their team must guess where the hidden target lies based on the clue's interpretation, resulting in amusing and insightful discussions.",
+        "url": "https://longwave.web.app/",
+        "minimumPlayers": 2
+    },
+    {
+        "name": "One Word",
+        "description": "A cooperative word-based party game where players collaborate to construct a coherent story, each contributing only one word at a time. The goal is to create a narrative that flows smoothly despite the limited input from each player, often leading to unexpected and entertaining twists.",
+        "url": "https://oneword.games/",
+        "minimumPlayers": 3
+    },
+    {
+        "name": "Skribbl",
+        "description": "An online multiplayer drawing and guessing game where players take turns drawing a given word while others try to guess what it is. The game's fun and fast-paced nature, along with the humorous interpretations of drawings, make it a popular choice for virtual gatherings and casual gameplay.",
+        "url": "https://skribbl.io",
+        "minimumPlayers": 2,
+        "maximumPlayers": 20
+    },
+    {
+        "name": "Spyfall",
+        "description": "A social deduction game where players are given secret roles as either regular players or a spy, except one player doesn't know their role. Set in various locations, players ask and answer questions to figure out who the spy is, while the spy attempts to blend in and identify the location without revealing their lack of knowledge.",
+        "url": "https://www.spyfall.app/",
+        "minimumPlayers": 4,
+        "maximumPlayers": 14
+    },
+    {
+        "name": "WikiRace",
+        "description": "An online game where players race against each other to navigate from one Wikipedia article to another using only the links within the articles. The goal is to find the shortest path between the initial and target articles, showcasing players' knowledge of Wikipedia's vast interconnectedness.",
+        "url": "https://wiki-race.com/",
+        "minimumPlayers": 1,
+        "maximumPlayers": 10
+    }
+];
+
+export default games;
